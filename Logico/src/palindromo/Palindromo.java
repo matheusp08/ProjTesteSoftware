2cbe6d906ab13a21913eaa7921de947557f82178
Igor Fonseca <igorcpf@gmail.com>
2017-06-03 00:32:22 -0300

    adding some prints to make debug easier

--- Logico/src/palindromo/Palindromo.java
+++ Logico/src/palindromo/Palindromo.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Palindromo {
 
 	public boolean isPalindromo(String palavra) {
-		System.out.print("Caminho: [1-");
 		Scanner sc = new Scanner(palavra);
 		boolean isPalindromo = false;
 		String letra, resposta;
@@ -16,45 +15,38 @@ public class Palindromo {
 		contador_texto = 0;
 
 		sc.useDelimiter("");
-		System.out.print("2-");
 		while (sc.hasNext() && contador_entrada <= 10) {
-			System.out.print("3-");
+			System.out.println("P1-C1-C2-C3");
 			letra = sc.next();
 			if (!letra.equals(" ")) {
-				System.out.print("4-");
+				System.out.println("P2-C4");
 				texto[contador_texto] = letra.charAt(0);
 				contador_texto++;
 			}
 			contador_entrada++;
-			System.out.print("5-2-");
 		}
-		System.out.print("6-");
 		i = 0;
 		j = contador_texto - 1;
 		palindromo = 1;
 		
-		System.out.print("7-");
 		while (j > i) {
-			System.out.print("8-");
+			System.out.println("P3-C5");
 			if (texto[i] != texto[j]) {
-				System.out.print("9-");
+				System.out.println("P4-C6");
 				palindromo = 0;
 			}
 			j = j - 1;
 			i = i + 1;
-			System.out.print("10-7-");
 		}
-		System.out.print("11-");
 		if (palindromo == 1) {
-			System.out.print("12-");
+			System.out.println("P5-C7");
 			isPalindromo = true;
 			resposta = "SIM";
 		} else {
-			System.out.print("13-");
+			System.out.println("!P5-!C7");
 			isPalindromo = false;
 			resposta = "NAO";
 		}
-		System.out.println("14]");
 		sc.close();
 		System.out.println("Resultado obtido: " + resposta);
 		return isPalindromo;
--- Logico/src/palindromo/PalindromoTest.java
+++ Logico/src/palindromo/PalindromoTest.java
@@ -21,43 +21,43 @@ public class PalindromoTest {
 
 	@Test
 	public void TestCase2() {
-		System.out.println("TEST 2:");
+		System.out.println("\nTEST 2:");
 		Assert.assertFalse(palindromo.isPalindromo("teste"));
 	}
 	
 	@Test
 	public void TestCase3() {
-		System.out.println("TEST 3:");
+		System.out.println("\nTEST 3:");
 		Assert.assertTrue(palindromo.isPalindromo(" "));
 	}
 	
 	@Test
 	public void TestCase4() {
-		System.out.println("TEST 4:");
+		System.out.println("\nTEST 4:");
 		Assert.assertTrue(palindromo.isPalindromo(""));
 	}
 	
 	@Test
 	public void TestCase5() {
-		System.out.println("TEST 5:");
+		System.out.println("\nTEST 5:");
 		Assert.assertFalse(palindromo.isPalindromo("a b"));
 	}
 	
 	@Test
 	public void TestCase6() {
-		System.out.println("TEST 6:");
+		System.out.println("\nTEST 6:");
 		Assert.assertTrue(palindromo.isPalindromo("a"));
 	}
 	
 	@Test
 	public void TestCase7() {
-		System.out.println("TEST 7:");
+		System.out.println("\nTEST 7:");
 		Assert.assertFalse(palindromo.isPalindromo("bcca"));
 	}
 	
 	@Test
 	public void TestCase8() {
-		System.out.println("TEST 8:");
+		System.out.println("\nTEST 8:");
 		Assert.assertFalse(palindromo.isPalindromo("ab "));
 	}
 }
