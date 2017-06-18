ee2dbb694da0f74b5a37bb159f13a12dcf49ae30
Igor Fonseca <igorcpf@gmail.com>
2017-06-18 01:22:43 -0300

    tests janelaC2

--- Mutacao/src/teste/JanelaC2Test.java
+++ Mutacao/src/teste/JanelaC2Test.java
@@ -17,6 +17,52 @@ public class JanelaC2Test {
 	@Test
 	public void TestCase01() {
 		System.out.println("TEST 1:");
-		Assert.assertEquals(3,janelaC2.calculaJanela(1,2,3));
+		int resultado = janelaC2.calculaJanela(1,2,3);
+		
+		System.out.println("Resultado Obtido: " + resultado);
+		Assert.assertEquals(39800, resultado);
 	}
+	
+	  @Test
+	  public void TestCase02() {
+	    System.out.println("TEST 2:");
+	    
+	    int resultado = janelaC2.calculaJanela(100,200,300);
+	    
+	    System.out.println("Resultado Obtido: " + resultado);
+	    
+	    Assert.assertEquals(20000,resultado);
+	  }
+	  @Test
+	  public void TestCase03() {
+	    System.out.println("TEST 3:");
+	    
+	    int resultado = janelaC2.calculaJanela(100,199,400);
+	    
+	    System.out.println("Resultado Obtido: " + resultado);
+	    
+	    Assert.assertEquals(10100,resultado);
+	  }
+	  
+	  @Test
+	  public void TestCase04() {
+	    System.out.println("TEST 4:");
+	    
+	    int resultado = janelaC2.calculaJanela(0,300,400);
+	    
+	    System.out.println("Resultado Obtido: " + resultado);
+	    
+	    Assert.assertEquals(10000,resultado);
+	  }
+	  
+	  @Test
+	  public void TestCase05() {
+	    System.out.println("TEST 5:");
+	    
+	    int resultado = janelaC2.calculaJanela(0,200,400);
+	    
+	    System.out.println("Resultado Obtido: " + resultado);
+	    
+	    Assert.assertEquals(0,resultado);
+	  }
 }
--- Mutacao/src/teste/JanelaCTest.java
+++ Mutacao/src/teste/JanelaCTest.java
@@ -7,16 +7,65 @@ import janela.JanelaC;
 
 public class JanelaCTest {
 
-	private JanelaC janelaC;
+  private JanelaC janelaC;
 
-	@Before
-	public void setup() {
-		janelaC = new JanelaC(); 
-	}
+  @Before
+  public void setup() {
+    janelaC = new JanelaC(); 
+  }
 
-	@Test
-	public void TestCase01() {
-		System.out.println("TEST 1:");
-		Assert.assertEquals(3,janelaC.calculaJanela(1,2,3));
-	}
+  @Test
+  public void TestCase01() {
+    System.out.println("TEST 1:");
+    
+    int resultado = janelaC.calculaJanela(0,0,0);
+    
+    System.out.println("Resultado Obtido: " + resultado);
+    
+    Assert.assertEquals(40000,resultado);
+  }
+  
+  @Test
+  public void TestCase02() {
+    System.out.println("TEST 2:");
+    
+    int resultado = janelaC.calculaJanela(100,200,300);
+    
+    System.out.println("Resultado Obtido: " + resultado);
+    
+    Assert.assertEquals(20000,resultado);
+  }
+  
+  @Test
+  public void TestCase03() {
+    System.out.println("TEST 3:");
+    
+    int resultado = janelaC.calculaJanela(400,400,400);
+    
+    System.out.println("Resultado Obtido: " + resultado);
+    
+    Assert.assertEquals(40000,resultado);
+  }
+  
+  @Test
+  public void TestCase04() {
+    System.out.println("TEST 4:");
+    
+    int resultado = janelaC.calculaJanela(0,200,400);
+    
+    System.out.println("Resultado Obtido: " + resultado);
+    
+    Assert.assertEquals(0,resultado);
+  }
+  
+  @Test
+  public void TestCase05() {
+    System.out.println("TEST 5:");
+    
+    int resultado = janelaC.calculaJanela(1,1,390);
+    
+    System.out.println("Resultado Obtido: " + resultado);
+    
+    Assert.assertEquals(20000,resultado);
+  }
 }
